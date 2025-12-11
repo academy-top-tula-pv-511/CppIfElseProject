49a41123bc4e4292aff38dc5d78b94301b8ce51b
MaxEfimov <efimov@mail.ru>
2025-12-11 21:13:44 +0300

    Добавьте файлы проекта.

---
+++ CppIfElseProject/CppIfElseProject.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    if (n < 0)
+    {
+        n = -n;
+    }
+        
+
+    if (!(n % 2))
+    {
+        std::cout << "even\n";
+        n = n * 2;
+    }
+    else
+    {
+        std::cout << "odd\n";
+        n = n * 3;
+    }
+}
