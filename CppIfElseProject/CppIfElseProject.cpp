220f0df03bdcbbe8651a7c6248d6558958ec8206
MaxEfimov <efimov@mail.ru>
2025-12-16 19:27:26 +0300

    16-12-25

--- CppIfElseProject/CppIfElseProject.cpp
+++ CppIfElseProject/CppIfElseProject.cpp
@@ -1,8 +1,10 @@
 ﻿#include <iostream>
 
+int global{ 500 };
+
 int main()
 {
-    int n;
+    /*int n;
     std::cin >> n;
 
     if (n < 0)
@@ -10,7 +12,6 @@ int main()
         n = -n;
     }
         
-
     if (!(n % 2))
     {
         std::cout << "even\n";
@@ -20,5 +21,33 @@ int main()
     {
         std::cout << "odd\n";
         n = n * 3;
+    }*/
+
+
+    int a{ 20 };
+    std::cout << a << "\n";
+    int b{ 40 };
+
+    const int maxSpeed{ 300 };
+    const int FileNotFound{ 123 };
+    const double PI{ 3.14156 };
+
+    int errorCode{};
+    //
+    // app code
+    //
+
+    if (errorCode == FileNotFound)
+    {
+        // reaction
     }
+
+
+
+    /*{
+        a = 30;
+        std::cout << b << "\n";
+    }*/
+    
+    std::cout << global << "\n";
 }
